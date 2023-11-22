44f3aa3cbc809de5bc523d83a2b410ea91c99980
AryssonLuz <arysson2020@gmail.com>
2023-11-22 13:56:43 -0300

    algumas mudanças

--- main.c
+++ main.c
@@ -3,243 +3,190 @@
 #include <string.h>
 #include <locale.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+typedef struct Velha {
+    int i;
+    int j;
+    int linha;
+    int coluna;
+    int jogador;
+    int ganhou;
+    int jogadas;
+    int opcao;
+    int op;
+    char tabu[3][3];
+} Velha;
+
 int main(int argc, char *argv[]) {
-	
-	//area da declaração das variaveis
-	setlocale(LC_ALL, "Portuguese_Brazil");
-	int i,j,linha,coluna, jogador, ganhou, jogadas, opcao, op;
-	char tabu[3][3];
-	FILE *rak;
-	
-	struct jogadores{
-		char nome[500];
-		int id;
-	};
-	
-	struct jogadores jo;
-	
-	do{
-		//aqui ele irar fazer imprimir todo o menu
-		printf("\n\t-> Jogo da velha <-\n");
-		printf("\n\n\n\tDigite (1) para jogar\n\n\tDigite (2) para Ver os creditos\n\n\tDigite (3) para sair\n\n");
-		scanf("%d",&op);
-		//o system será usado para que as informações apresentadas anteriomente não fique na tela, deixando assim mais dinamico
-		system("cls");
-		
-			//dando a possibilidade de poder escolher as opções apresentadas anteriormente
-			switch(op){
-				case 1:{
-					
-							do{
-						jogador = 1;
-						ganhou = 0;
-						jogadas = 0;
-						
-						//criação do tabuleiro
-						for (i=0;i<3;i++){
-							
-							for(j=0;j<3;j++){
-								
-								tabu[i][j]=' ';
-											
-							}
-							
-						}
-						
-						
-						do{
-							//as coordenadas do tabuleiro, que ira reprentar a localização das possiveis jogadas
-							printf("\n\n\t 1   2   3\n\n");
-							for (i=0;i<3;i++){
-								
-								for(j=0;j<3;j++){
-									if(j == 0){
-										printf("\t");
-									}
-									
-									printf(" %c",tabu[i][j]);
-									
-									if(j < 2){
-									
-										printf(" |");
-									}
-									if(j == 2){
-										
-										printf("   %d",i+1);
-									}
-										
-								}
-								if(i < 2){
-									printf("\n\t-----------");
-								}
-								printf("\n");
-								
-							}
-							do{
-							printf("\n\n  Jogador %d: Digite a linha e a coluna que deseja jogar:\n",jogador);
-							scanf("%d%d",&linha,&coluna);
-							linha= linha - 1;
-							coluna = coluna- 1;
-							if(linha < 0 || linha > 2 || coluna < 0 || coluna > 2 || tabu[linha][coluna] != ' '){
-								
-							}else{
-								
-								system("cls");
-							}
-							
-							}while(linha < 0 || linha > 2 || coluna < 0 || coluna > 2 || tabu[linha][coluna] != ' ' );
-						
-							if(jogador == 1){
-								tabu [linha][coluna] = '0';	
-								jogador ++;
-							}else{	
-								tabu [linha][coluna] = 'X';
-								jogador=1;
-							}
-							
-							jogadas++;
-							
-							if(tabu [0][0] == '0' && tabu [0][1] == '0' && tabu [0][2] == '0' ||
-								tabu [1][0] == '0' && tabu [1][1] == '0' && tabu [1][2] == '0' ||
-								tabu [2][0] == '0' && tabu [2][1] == '0' && tabu [2][2] == '0'){
-									printf("\nParabens!!! Jogador 1 venceu!!!\n");
-									getchar();
-									ganhou = 1;
-								}
-								
-							if(tabu [0][0] == 'X' && tabu [0][1] == 'X' && tabu [0][2] == 'X' ||
-								tabu [1][0] == 'X' && tabu [1][1] == 'X' && tabu [1][2] == 'X' ||
-								tabu [2][0] == 'X' && tabu [2][1] == 'X' && tabu [2][2] == 'X'){
-									printf("\nParabens!!! Jogador 2 venceu!!!\n");
-									getchar();
-									ganhou = 1;
-								}
-								
-							if(tabu [0][0] == '0' && tabu[1][0] == '0' && tabu [2][0] == '0' ||
-								tabu [0][1] == '0' && tabu[1][1] == '0' && tabu [2][1] == '0'||
-								tabu [0][2] == '0' && tabu[1][2] == '0' && tabu [2][2] == '0'){
-									printf("\nParabens!!! Jogador 1 venceu!!!\n");
-									getchar();
-									ganhou = 1;
-									}	
-									
-							if(tabu [0][0] == 'X' && tabu[1][0] == 'X' && tabu [2][0] == 'X' ||
-								tabu [0][1] == 'X' && tabu[1][1] == 'X' && tabu [2][1] == 'X'||
-								tabu [0][2] == 'X' && tabu[1][2] == 'X' && tabu [2][2] == 'X'){
-									printf("\nParabens!!! Jogador 2 venceu!!!\n");
-									getchar();
-									ganhou = 1;
-									}	
-							
-							if(tabu [0][0] == '0' && tabu[1][1] == '0' && tabu [2][2] == '0'){
-								printf("\nParabens!!! Jogador 1 venceu!!!\n");
-								getchar();
-								ganhou = 1;
-							}
-							
-							if(tabu [0][0] == 'X' && tabu[1][1] == 'X' && tabu [2][2] == 'X'){
-								printf("\nParabens!!! Jogador 2 venceu!!!\n");
-								getchar();
-								ganhou = 1;
-							}
-							
-							if(tabu [0][2] == '0' && tabu[1][1] == '0' && tabu [2][0] == '0'){
-								printf("\nParabens!!! Jogador 1 venceu!!!\n");
-								getchar();
-								ganhou = 1;
-							}
-							
-							if(tabu [0][2] == 'X' && tabu[1][1] == 'X' && tabu [2][0] == 'X'){
-								printf("\nParabens!!! Jogador 2 venceu!!!\n");
-								getchar();
-								ganhou = 1;
-							}
-						}while(ganhou == 0 && jogadas < 9);
-						
-						if((ganhou == 1 && jogador == 1) || (ganhou == 1 && jogador == 2)){
-									
-							rak= fopen("Ranking.txt", "r");
-							
-							if(rak == NULL){
-								jo.id=1;
-							}else{
-								jo.id++;	
-							}
-																																
-							rak = fopen("Ranking.txt", "a");
-							
-							if(rak == NULL){
-								printf("Erro ao abrir o arquivo!\n");
-								getchar();
-							}
-							
-							printf("\nDigite seu nome, ganhador:");
-							scanf("%s",jo.nome);	
-					
-							
-							fprintf(rak,"\n%d:%s. Ganhou!!!",jo.id,jo.nome);
-								
-							fclose(rak);
-								
-							printf("Seu nome foi gravado com sucesso!!!");
-							
-						    jogadas = 0;
-						}
-						
-						if(ganhou == 0){
-							system("cls");
-							printf("O jogo finalizou sem ganhador!"); 	
-							printf("\nDigite 1 para jogar novamente: ");
-							printf("\nDigite qualquer outra coisa para sair: ");
-							scanf("%d",&opcao);
-							
-						}
-						
-						if(ganhou == 1){
-							system("cls");
-							printf("O jogo acabou, deseja ir outra partida?");
-							printf("\nDigite 1 para jogar novemente: ");
-							printf("\nDigite qualquer outra coisa para sair: ");
-							scanf("%d",&opcao);
-						}
-						
-						if(opcao > 1){
-							rak = fopen("Ranking.txt","a");
-							fprintf(rak,"\n----------------------------------\n\n");
-							
-						}
-						
-					}while(opcao == 1);
-					
-					break;
-				}
-				case 2:{
-					
-					printf("Criador por:\nArysson André\nKaike Garcia\nAnthonny John\n\n\nPrecione qualquer tecla: ");
-					getchar();					
-					
-					break;
-				}
-				case 3:{
-					
-					printf("Fechando jogo!");
-					getchar();
-					exit(0);
-					break;
-				}
-				
-				default:{
-					
-					printf("Opcao invalida");
-					system("cls");
-					
-					break;
-				}
-			}
-		getchar();
-		system("cls");
-		}while(op != 1);
-	
-	return 0;
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    Velha jogo;
+
+    do {
+        printf("\n\t-> Jogo da velha <-\n");
+        printf("\n\n\n\tDigite (1) para jogar\n\n\tDigite (2) para Ver os créditos\n\n\tDigite (3) para sair\n\n");
+        scanf("%d", &jogo.op);
+
+        while (getchar() != '\n');  
+        system("cls");
+
+        switch (jogo.op) {
+            case 1: {
+                do {
+                    jogo.jogador = 1;
+                    jogo.ganhou = 0;
+                    jogo.jogadas = 0;
+
+                    for (jogo.i = 0; jogo.i < 3; jogo.i++) {
+                        for (jogo.j = 0; jogo.j < 3; jogo.j++) {
+                            jogo.tabu[jogo.i][jogo.j] = ' ';
+                        }
+                    }
+
+                    do {
+                        printf("\n\n\t 1   2   3\n\n");
+                        for (jogo.i = 0; jogo.i < 3; jogo.i++) {
+                            for (jogo.j = 0; jogo.j < 3; jogo.j++) {
+                                if (jogo.j == 0) {
+                                    printf("\t");
+                                }
+                                printf(" %c", jogo.tabu[jogo.i][jogo.j]);
+                                if (jogo.j < 2) {
+                                    printf(" |");
+                                }
+                                if (jogo.j == 2) {
+                                    printf("   %d", jogo.i + 1);
+                                }
+                            }
+                            if (jogo.i < 2) {
+                                printf("\n\t-----------");
+                            }
+                            printf("\n");
+                        }
+
+                        do {
+                            printf("\n\n  Jogador %d: Digite a linha e a coluna que deseja jogar:\n", jogo.jogador);
+                            scanf("%d%d", &jogo.linha, &jogo.coluna);
+
+                            while (getchar() != '\n');  
+
+                            jogo.linha = jogo.linha - 1;
+                            jogo.coluna = jogo.coluna - 1;
+                            if (jogo.linha < 0 || jogo.linha > 2 || jogo.coluna < 0 || jogo.coluna > 2 ||
+                                jogo.tabu[jogo.linha][jogo.coluna] != ' ') {
+                                printf("Jogada inválida. Tente novamente.\n");
+                            } else {
+                                system("cls");
+                            }
+                        } while (jogo.linha < 0 || jogo.linha > 2 || jogo.coluna < 0 || jogo.coluna > 2 ||
+                                 jogo.tabu[jogo.linha][jogo.coluna] != ' ');
+
+                        if (jogo.jogador == 1) {
+                            jogo.tabu[jogo.linha][jogo.coluna] = '0';
+                            jogo.jogador++;
+                        } else {
+                            jogo.tabu[jogo.linha][jogo.coluna] = 'X';
+                            jogo.jogador = 1;
+                        }
+
+                        jogo.jogadas++;
+
+                        if (jogo.tabu[0][0] == '0' && jogo.tabu[0][1] == '0' && jogo.tabu[0][2] == '0' ||
+                            jogo.tabu[1][0] == '0' && jogo.tabu[1][1] == '0' && jogo.tabu[1][2] == '0' ||
+                            jogo.tabu[2][0] == '0' && jogo.tabu[2][1] == '0' && jogo.tabu[2][2] == '0') {
+                            printf("\nParabéns!!! Jogador 1 venceu!!!\n");
+                            getchar();
+                            jogo.ganhou = 1;
+                        }
+
+                        if (jogo.tabu[0][0] == 'X' && jogo.tabu[0][1] == 'X' && jogo.tabu[0][2] == 'X' ||
+                            jogo.tabu[1][0] == 'X' && jogo.tabu[1][1] == 'X' && jogo.tabu[1][2] == 'X' ||
+                            jogo.tabu[2][0] == 'X' && jogo.tabu[2][1] == 'X' && jogo.tabu[2][2] == 'X') {
+                            printf("\nParabéns!!! Jogador 2 venceu!!!\n");
+                            getchar();
+                            jogo.ganhou = 1;
+                        }
+
+                        if (jogo.tabu[0][0] == '0' && jogo.tabu[1][0] == '0' && jogo.tabu[2][0] == '0' ||
+                            jogo.tabu[0][1] == '0' && jogo.tabu[1][1] == '0' && jogo.tabu[2][1] == '0' ||
+                            jogo.tabu[0][2] == '0' && jogo.tabu[1][2] == '0' && jogo.tabu[2][2] == '0') {
+                            printf("\nParabéns!!! Jogador 1 venceu!!!\n");
+                            getchar();
+                            jogo.ganhou = 1;
+                        }
+
+                        if (jogo.tabu[0][0] == 'X' && jogo.tabu[1][0] == 'X' && jogo.tabu[2][0] == 'X' ||
+                            jogo.tabu[0][1] == 'X' && jogo.tabu[1][1] == 'X' && jogo.tabu[2][1] == 'X' ||
+                            jogo.tabu[0][2] == 'X' && jogo.tabu[1][2] == 'X' && jogo.tabu[2][2] == 'X') {
+                            printf("\nParabéns!!! Jogador 2 venceu!!!\n");
+                            getchar();
+                            jogo.ganhou = 1;
+                        }
+
+                        if (jogo.tabu[0][0] == '0' && jogo.tabu[1][1] == '0' && jogo.tabu[2][2] == '0') {
+                            printf("\nParabéns!!! Jogador 1 venceu!!!\n");
+                            getchar();
+                            jogo.ganhou = 1;
+                        }
+
+                        if (jogo.tabu[0][0] == 'X' && jogo.tabu[1][1] == 'X' && jogo.tabu[2][2] == 'X') {
+                            printf("\nParabéns!!! Jogador 2 venceu!!!\n");
+                            getchar();
+                            jogo.ganhou = 1;
+                        }
+
+                        if (jogo.tabu[0][2] == '0' && jogo.tabu[1][1] == '0' && jogo.tabu[2][0] == '0') {
+                            printf("\nParabéns!!! Jogador 1 venceu!!!\n");
+                            getchar();
+                            jogo.ganhou = 1;
+                        }
+
+                        if (jogo.tabu[0][2] == 'X' && jogo.tabu[1][1] == 'X' && jogo.tabu[2][0] == 'X') {
+                            printf("\nParabéns!!! Jogador 2 venceu!!!\n");
+                            getchar();
+                            jogo.ganhou = 1;
+                        }
+                    } while (jogo.ganhou == 0 && jogo.jogadas < 9);
+
+                    if (jogo.ganhou == 0) {
+                        system("cls");
+                        printf("O jogo finalizou sem ganhador!\n");
+                        printf("Digite 1 para jogar novamente: ");
+                        printf("Digite qualquer outra coisa para sair: ");
+                        scanf("%d", &jogo.opcao);
+                    }
+
+                    if (jogo.ganhou == 1) {
+                        system("cls");
+                        printf("O jogo acabou, deseja ir outra partida?\n");
+                        printf("Digite 1 para jogar novamente ou digite qualquer outra coisa para sair: ");
+                        scanf("%d", &jogo.opcao);
+                        
+                    }
+
+                } while (jogo.opcao == 1);
+
+                break;
+            }
+            case 2: {
+                printf("\n\tCriado por:\n\n\tArysson André\n\n\tKaike Garcia\n\n\tAnthonny John\n\n\nPressione qualquer tecla: ");
+                break;
+            }
+            case 3: {
+                printf("Fechando jogo!\n");
+                exit(0);
+                break;
+            }
+            default: {
+                printf("Opção inválida\n");
+                system("cls");
+                break;
+            }
+        }
+
+        while (getchar() != '\n'); 
+        system("cls");
+    } while (jogo.op != 1);
+
+    return 0;
 }
+
